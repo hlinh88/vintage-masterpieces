0388ed3d4e5aa58a7f7a34d5766ada4487c18b3f
Hoang Linh <hlinh.jobs@gmail.com>
2024-03-12 00:32:24 +0700

    Hide api keys

--- Vintage_Masterpieces/Vintage_Masterpieces/Service/APIConstants.swift
+++ Vintage_Masterpieces/Vintage_Masterpieces/Service/APIConstants.swift
@@ -10,8 +10,8 @@ import Foundation
 enum APIConstants {
     static let apiHost = "api.spotify.com"
     static let authHost = "accounts.spotify.com"
-    static let clientId = "ca09a48bcdfd460b986cacad620d317b"
-    static let clientSecret = "f7511b1377684280b168a74b006eeee3"
+    static let clientId = {clientId}
+    static let clientSecret = {clientSecret}
     static let redirectUri = "https://www.google.com"
     static let responseType = "token"
     static let scopes = "user-read-private"
